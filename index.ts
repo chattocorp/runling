81c8a0a9c7d95e2f1100a2c47b807be34cb4e032
Hendrik Mans <hendrik@mans.de>
2026-09-02 11:31:11 +0200

    chore: actually talk to pi

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { $ } from "bun";
+
+function pi(prompt: string) {
+  return $`pi -p ${prompt}`.text();
+}
+
+const prompt = "Write a funny joke!";
+const result = await pi(prompt);
+
+console.log(result);
